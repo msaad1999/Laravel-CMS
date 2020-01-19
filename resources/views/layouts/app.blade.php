625f440ed4c8a3bb74e4d2498ac1c3ce6fafdb1f
Muhammad Saad <saad01.1999@gmail.com>
2020-01-20 02:03:09 +0500

    create and connect base layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel CMS') }}</title>
+
+    <!-- Favicon -->
+    <link href="{{ asset('img') }}/favicon.png" rel="icon" type="image/png">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="{{ asset('css/libs.css') }}" rel="stylesheet" type="text/css">
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @auth()
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            @include('layouts.navbars.sidebar')
+        @endauth
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+                @include('layouts.navbars.navbar')
+                @yield('content')
+            </div>
+
+            @include('layouts.footers.auth')
+
+        </div>
+
+    </div>
+
+    @auth
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+    @endauth
+
+    <script src="{{ asset('js/libs.js') }}"></script>
+
+</body>
+
+</html>
