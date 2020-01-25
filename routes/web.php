59a627085c88f8ac27b5faec95c3b0f19b392c52
Muhammad Saad <saad01.1999@gmail.com>
2020-01-25 20:07:14 +0500

    create base system for comments feature

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,8 @@ Route::group(['middleware'=>'admin'], function(){
     Route::resource('admin/categories', 'AdminCategoriesController');
 
     Route::resource('admin/media', 'AdminMediaController');
+
+    Route::resource('admin/comments/', 'PostCommentsController');
+
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
 });
